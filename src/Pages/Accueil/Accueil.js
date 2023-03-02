152604ba91b7a976339b143c5337b799d292a982
bazrajae <baz.rajae1997@gmail.com>
2023-03-02 12:15:14 +0100

    msg2

--- src/Pages/Accueil/Accueil.js
+++ src/Pages/Accueil/Accueil.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import Menu from '../../Menu';
-
+import React from "react";
+import Menu from "../../Menu";
+import acc1 from "../../images/acc1.jpg";
 
 const Accueil = () => {
-    return (
-        <div>
-            <Menu/>
-            <h1>Accueil</h1>
-           <p>GOÛTEZ LES DÉLICEUSES PÂTES FRAÎCHES RÉALISÉES CHAQUE JOUR
-              DANS LE RESPECT DE LA TRADITION ITALIENNE PAR NOTRE CHEF</p>
-              <p>SO PASTA</p>
-        </div>
-    );
+  return (
+    <div>
+      <Menu />
+     
+      <section class="services-sections">
+        <div class="containers">
+
+     <div class="b1"><img src={acc1} width="50%" height="auto" alt="rajae" /></div>
+
+      <div class="b2">
+      <span class="R">piatto</span>
+      <div class="r3">DÉCOUVREZ LA SAVEUR DES PÂTES FRAÎCHES ISSUES DE NOTRE SAVOIR-FAIRE
+      TRADITIONNEL</div>
+       <p>Antipasto, primo et/ou dolce : notre carte s'appuie sur les
+      incontournables de la gastronomie italienne. Savourez nos pâtes fraîches &
+      al dente cuisinées dans le respect de la tradition du pays natal de notre
+      chef. Importés directement d'Italie, nos assortiments de fromage et de
+      charcuterie sont sélectionnés pour vous offrir un voyage culinaire sans
+      frontière. Composez vous-même votre assiette en choisissant vos pâtes et
+      votre sauce ou laissez-vous transporter par l'une des spécialités de notre
+      chef.</p>
+      </div>
+      </div>
+      </section>
+    </div>
+    
+  );
 };
 
 export default Accueil;
--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import Menu from '../../Menu';
-import jps from '../../images/jps.jpg';
-
-
+import React from "react";
+import Menu from "../../Menu";
+import jps from "../../images/jps.jpg";
 
 const Contact = () => {
-    return (
-        <div >
-           <Menu/>
-            <h1>À QUELQUES PAS DE LA GRAND PLACE </h1>
-            <div class="B">
-            <p>C'est à travers notre convivialité, notre savoir-faire traditionnel
-                 et l'utilisation de produits frais que nous vous faisons découvrir
-                  notre spécialité : les pâtes fraîches aux œufs. 
-                  Toute la famille vous accueille dans un cadre moderne
-                   et chaleureux pour vous faire profiter de la culture
-                    et de cuisine italienne.</p><br></br>
+  return (
+    <div>
+      <Menu />
+      <h1>À QUELQUES PAS DE LA GRAND PLACE </h1>
+      <div class="B">
+        <p>
+          C'est à travers notre convivialité, notre savoir-faire traditionnel et
+          l'utilisation de produits frais que nous vous faisons découvrir notre
+          spécialité : les pâtes fraîches aux œufs. Toute la famille vous
+          accueille dans un cadre moderne et chaleureux pour vous faire profiter
+          de la culture et de cuisine italienne.
+        </p>
+        <br></br>
 
-<p>Pour toute demande de réservation, nous vous conseillons de nous contacter au minimum 2 à 3 jours à l’avance.</p>
-
-           </div>
-           </div>
-                 
-        
-    );
+        <p>
+          Pour toute demande de réservation, nous vous conseillons de nous
+          contacter au minimum 2 à 3 jours à l’avance.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
